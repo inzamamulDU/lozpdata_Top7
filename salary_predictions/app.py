bf9d7021977ef4f654cd89009a39eafb6fb3a253
Taher Irfan <taherhphs@gmail.com>
2020-07-05 00:25:28 +0600

    app.py updated

--- salary_predictions/app.py
+++ salary_predictions/app.py
@@ -9,6 +9,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
 import re
 
+#from test.predict import pre_processor
+#from test.predict.py import pre_processor, feature_extractor, get_pipeline
 
 
 app=Flask(__name__)
@@ -20,7 +22,31 @@ company = list(company[1:].values.tolist())
 import pickle
 import nltk
 nltk.download('wordnet')
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
+def pre_processor(text):
+        return re.sub(r'[^0-9a-zA-Z]+', " ", text).lower()
+    
+def feature_extractor():
+    features = [('FullDescription-Bag of Words', 'FullDescription', vectorizor),
+                ('Title-Bag of Words', 'Title', vectorizor),
+                ('LocationRaw-Bag of Words', 'LocationRaw', vectorizor),
+                ('LocationNormalized-Bag of Words', 'LocationNormalized', vectorizor)]
+    combined = FeatureMapper(features)
+    return combined
 
+def get_pipeline():
+    features = feature_extractor()
+    steps = [("extract_features", features),
+             ("classify", RandomForestRegressor(n_estimators=50, 
+                                                verbose=2,
+                                                n_jobs=4,
+                                                min_samples_split=30,
+                                                random_state=3465343))]
+    return Pipeline(steps)
 filename = './test/data/random_forest.pickle'
 regressor = pickle.load( open(filename,"rb"))
 
